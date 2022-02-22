1b87188ffb75efe77f029bbc892e9532e3cb4961
hzn2020 <h737277953@163.com>
2022-02-23 00:26:07 +0800

    Initial commit

---
+++ git01/src/main/java/com/hzn/model/User.java
@@ -0,0 +1,14 @@
+package com.hzn.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Hzn
+ * @date 2022/2/23
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = -6671035697712825300L;
+
+    private Integer id;
+    private String name;
+}
